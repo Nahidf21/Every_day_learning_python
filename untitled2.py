56ddf20c65bc11382d509a566e6526218a0092f9
Nahid Ferdous <94561704+Nahidf21@users.noreply.github.com>
2023-05-28 20:55:58 -0500

    Add files via upload

---
+++ untitled2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 27 23:14:21 2023
+
+@author: nahidferdous
+"""
+def nahid(a,b,c):
+    print(a)
+    print(b)
+    print(c)
+my_list= [1,2,3]
+
+nahid(*my_list)
