69b1ee89dc1fd3bd8e190d7c3d80eae17e485b8a
Nahid Ferdous <94561704+Nahidf21@users.noreply.github.com>
2023-06-19 23:58:47 -0500

    Add files via upload

---
+++ data_v4.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 19 03:00:01 2023
+
+@author: nahidferdous
+"""
+
+import matplotlib.pyplot as plt
+
+## how can we change the color of the refarance line
+plt.plot([3,6,9,12],[2,5,8,11],'g')
+plt.show()
+
+plt.plot([3,6,9,12],[2,5,8,11],'bs')
+plt.show()
+
+
+
+
+
+
+
